a6b75d7c1269ed0f0bfa35a8d56d19c51174c409
BitzyG <gv.itzelandrea@gmail.com>
2021-06-02 13:05:58 -0500

    corrección uso de html externo
    Eliminé html que había vinculado con el index.html inicial

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import cipher from './cipher.js';
 
 console.log(cipher);
 
-const checkBtn = document.getElementById('checkBtnA');
-  checkBtnA.addEventListener('click', ()) => {
+const checkBtnA = document.getElementById("checkBtnA");
+  checkBtnA.addEventListener('click', () => {
     window.location="cifrar.html";
-  };
+  });
+
+const checkBtnB = document.getElementById("checkBtnB");
+  checkBtnB.addEventListener('click', () => {
+    window.location="descifrar.html";
+  });
+
+function cifrar() {
+  let textoCifrar = document.getElementById("Text1").value;
+  let clave = document.getElementById("key").value;
+}
+
