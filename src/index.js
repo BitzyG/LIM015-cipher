b619499a9672efa5f51efcd1690ae898eeb17c9f
BitzyG <gv.itzelandrea@gmail.com>
2021-06-02 10:24:57 -0500

    Cambio de OnClik a AddEventListener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const checkBtn = document.getElementById('checkBtnA');
+  checkBtnA.addEventListener('click', ()) => {
+    window.location="cifrar.html";
+  };
