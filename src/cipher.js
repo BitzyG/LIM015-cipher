494a7c0afe4b08ffc6cfe43229270ecf772f4d1c
BitzyG <gv.itzelandrea@gmail.com>
2021-06-03 13:14:16 -0500

    comienzo mi archivo cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,9 @@
-const cipher = {
-  // ...
-};
+function cipher () {
+    let textoCifrar = document.getElementById("codigo").value;
+    let code = textoCifrar.chartCodeAt();
+    let key = document.getElementById("key").value;
+    let aCifrar = (code.chartCodeAt()- 65 + key)%26+65;
+  document.getElementById("cipher").innerHTML= aCifrar
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import cipher from './cipher.js';
-
-console.log(cipher);
+//console.log(cipher);
 
 document.getElementById("welcome").innerText = "BIENVENIDX"
 document.getElementById("textoAqui").innerText = "Por favor, introduce el texto que deseas Cifrar/Descifrar"
@@ -9,18 +8,21 @@ document.getElementById("key").innerText = "Por favor, indica la llave"
 document.getElementById("textoResultado").innerText = "Resultado:"
 
 function cifrar() {
-  let x = document.getElementById("codigo");
-  document.getElementById("resultado").innerHTML = x.value;
-  x.value= "";
-  x.innerHTML = "";
+  let c = document.getElementById("codigo");
+  document.getElementById("resultado").innerHTML = c.value;
+  c.value= "";
+  c.innerHTML = cipher.aCifrar
 }
 document.getElementById("btnCifrar").addEventListener("click",cifrar)
 
 function descifrar() {
-  let x = document.getElementById("codigo");
-  document.getElementById("resultado").innerHTML = x.value;
-  x.value= "";
-  x.innerHTML = "";
+  let d = document.getElementById("codigo");
+  document.getElementById("resultado").innerHTML = d.value;
+  d.value= "";
+  d.innerHTML = "";
 }
 document.getElementById("btnDescifrar").addEventListener("click",descifrar)
 
+
+//let c es codigo a cifrar
+//let c es codigo d descifrar
