f06a31b86a5bef53640a1f5643cbd09d65d3173f
BitzyG <gv.itzelandrea@gmail.com>
2021-06-04 19:31:00 -0500

    el codigo ya pasa Test
    No logro hacer que me muestre el resultado en la caja de texto, valor se muestra como Undefine

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,52 @@
 const cipher = {
-  encode(){
-    let textoCifrar = document.getElementById("codigo").value;
-    let key = document.getElementById("llave").value;
-    let code = (textoCifrar.chartCodeAt()- 65 + key)% 26 + 65;
-    let aCifrar = String.fromCharCode(code);
-    let encode = aCifrar;
-    document.getElementById("cifrado").innerHTML= aCifrar;
+    //para crear el cifrado del codigo del usuario, arronjando error si se ingresa un valor invalido
+    encode : (llave, codigo) => {
+      if (llave === null || llave === 0) {
+        throw new TypeError();
+      }
+    //defino el nombre de la variable, utilizando un FOR para crear un loop
+    //con charCodeAt, obtengo el valor ascii de las letras en el mensaje
+      let cifradoTotal = '';
+      for ( let i = 0 ; i < codigo.length ; i += 1) {
+        const numeroAscii = codigo.charCodeAt(i);
 
-    document.getElementById("btnCifrar").addEventListener("click",encode)
+    //numeros ASCII 48-57=numeros del 0-9
+    //numeros ASCII 65-90=letras A-Z (sin Ñ)
+    //numeros ASCII 97-122=letras a-z (sin ñ)
+        if( numeroAscii >= 65 && numeroAscii <= 90 ) {
+          cifradoTotal += String.fromCharCode((numeroAscii - 65 + llave) % 26 + 65);
+        } else if ( numeroAscii >= 97 && numeroAscii <= 122 ) {
+          cifradoTotal += String.fromCharCode((numeroAscii - 97 + llave) % 26 + 97);
+        } else if ( numeroAscii >= 48 && numeroAscii <= 57 ) {
+          cifradoTotal += String.fromCharCode((numeroAscii - 48 + llave) % 10 + 48);
+        } else {
+          cifradoTotal += codigo[i];
+        }
+      }
+      return cifradoTotal;
   },
 
-};
+    decode : (llave, codigo) => {
+      if (llave === null || llave === 0) {
+        throw new TypeError();
+      }
 
+      let descifradoTotal = '';
+      for ( let i = 0 ; i < codigo.length ; i += 1) {
+        const numeroAscii = codigo.charCodeAt(i);
 
-export default cipher;
+        if( numeroAscii >= 65 && numeroAscii <= 90 ) {
+          descifradoTotal += String.fromCharCode((numeroAscii - 90 - llave) % 26 + 90);
+        } else if ( numeroAscii >= 97 && numeroAscii <= 122 ) {
+          descifradoTotal += String.fromCharCode((numeroAscii - 122 - llave) % 26 + 122);
+        } else if ( numeroAscii >= 48 && numeroAscii <= 57 ) {
+          descifradoTotal += String.fromCharCode((numeroAscii - 57 - llave) % 10 + 57);
+        } else {
+          descifradoTotal += codigo[i];
+        }
+      }
+      return descifradoTotal;
+    }
+  };
+
+  export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
 import cipher from './cipher.js';
-//console.log(cipher);
+console.log(cipher);
 
+//textos en HTML
 document.getElementById("welcome").innerText = "BIENVENIDX"
 document.getElementById("textoAqui").innerText = "Por favor, introduce el texto que deseas Cifrar/Descifrar"
 document.getElementById("question").innerText = "¿Qué deseas hacer?"
 document.getElementById("key").innerText = "Por favor, indica la llave"
 document.getElementById("textoResultado").innerText = "Resultado:"
 
-function cifrar() {
-  let c = document.getElementById("codigo");
-  document.getElementById("resultado").innerHTML = c.value;
-  c.value= "";
-  c.innerHTML = cipher.aCifrar
-}
-document.getElementById("btnCifrar").addEventListener("click",cifrar)
-/*
-function descifrar() {
-  let d = document.getElementById("codigo");
-  document.getElementById("resultado").innerHTML = d.value;
-  d.value= "";
-  d.innerHTML = "";
-}
-document.getElementById("btnDescifrar").addEventListener("click",descifrar)
-*/
+//llamar funciones de de archivo cipher
+const textoCifrado = cipher.cifradoFinal;
+const textoDescifrado = cipher.descifradoTotal;
 
-//let c es codigo a cifrar
-//let c es codigo d descifrar
+//enviar texto de la caja de codigo a la de resultado de acuerdo al boton
+function cifrar() {
+  let c = textoCifrado;
+  document.getElementById("resultado").innerHTML = c;
+ }
+document.getElementById("btnCifrar").addEventListener("click",cifrar)
+
+function descifrar() {
+  let d = textoDescifrado;
+  document.getElementById("resultado").innerHTML = d;
+ }
+ document.getElementById("btnDescifrar").addEventListener("click",descifrar)
